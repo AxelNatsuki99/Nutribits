27f5aa027d3f42969ba1af693570e41191b5a3ea
Axel Adam <163078081+AxelNatsuki99@users.noreply.github.com>
2024-09-18 21:01:36 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+// Configuración de la conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "nutribits";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener el número de control desde el formulario
+$control_num = $_POST['control_num'];
+
+// Consulta para verificar si el usuario existe en la base de datos
+$sql = "SELECT * FROM usuarios WHERE username='$control_num'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Si el usuario es válido, redirige a principal.html
+    header("Location:  inicio.html");
+    exit();
+} else {
+    // Si el usuario no es válido, redirige de nuevo al login con un mensaje de error
+    header("Location: index.html?error=1");
+    exit();
+}
+
